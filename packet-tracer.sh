7d85f3ff0c0e9baa452eb42d2487b661a6f3c197
Brendon-Nunes <79028570+Brendon-Nunes@users.noreply.github.com>
2021-04-24 18:56:31 -0400

    Add files via upload

---
+++ packet-tracer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Ubuntu 20.04 LTS
+
+wget https://download1638.mediafire.com/aejezeu53sag/dz8q90i810n2f4i/PacketTracer_800_amd64_build212_final.deb
+
+sudo dpkg -i PacketTracer_800_amd64_build212_final.deb
+
+exit
